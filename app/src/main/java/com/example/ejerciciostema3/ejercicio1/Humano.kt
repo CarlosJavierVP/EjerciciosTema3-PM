04012f7604ceda2dc9d408bfe9bdb18a983fb63e
CarlosJavierValenzue <carlos.valenzuela.j92@gmail.com>
2024-09-25 13:40:23 +0200

    clase humano método mayor

--- app/src/main/java/com/example/ejerciciostema3/ejercicio1/Humano.kt
+++ app/src/main/java/com/example/ejerciciostema3/ejercicio1/Humano.kt
@@ -2,8 +2,17 @@ package com.example.ejerciciostema3.ejercicio1
 
 class Humano (var nombre:String, edad:Byte) : SerVivo(edad){
 
-    fun mayor(other: Humano){
-        super.mayor(other)
+    fun mayor(other: Humano) : Humano{
+        var result : Humano = Humano(this.nombre, this.edad)
+        if (other.edad == this.edad){
+            if (other.nombre.length > this.nombre.length){
+                result = Humano(other.nombre, other.edad)
+            }
+        }else if (other.edad > this.edad){
+            result = Humano(other.nombre, other.edad)
+        }
+
+        return result
     }
 
     fun equals(other: Humano): Boolean {
